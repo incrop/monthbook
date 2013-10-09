04d17732d6c13cc02a993834a30d418e700815cd
Dmitry Starostin <dmstarostin@gmail.com>
2013-10-09 17:21:50 +0400

    expanding/collapsing posts

--- source/js/app.js
+++ source/js/app.js
@@ -12,8 +12,6 @@ $(function () {
 		model: Student
 	});
 
-	var studentList = new StudentList();
-
 	var StudentView = Backbone.View.extend({
 		el: ".student",
 		initialize: function() {
@@ -26,13 +24,28 @@ $(function () {
 
 	var StudentListView = Backbone.View.extend({
 		el: "#content",
+		collection: new StudentList(),
+		events: {
+			"click .student .avatar, .student .caption": "toggleStudent"
+		},
+		toggleStudent: function (ev) {
+			var id = $(ev.currentTarget).closest(".student").data("id");
+			this.collection.get(id).toggle();
+		},
+		initialize: function() {
+			var self = this;
+			this.collection.on("change", function (model) {
+				self.$el.children("[data-id='" + model.id + "']")
+					.replaceWith(yr.run("students", { single: true, students: model.toJSON() }));
+			});
+		},
 		render: function () {
 			var self = this;
-			studentList.fetch({
+			this.collection.fetch({
 				success: function(studentList) {
 					var data = studentList.toJSON();
 					data[24].expanded = true;
-					self.$el.html(yr.run('students', { students: data }));
+					self.$el.html(yr.run("students", { students: data }));
 				}
 			});
 		}
